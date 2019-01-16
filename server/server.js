8f17ea8757a6879ef57bfd93f867608b6cb7ce02
igrepinc <igor.repinc@oroundo.com>
2019-01-16 12:32:44 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,13 +16,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Wellcome to chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+
+        /*
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        */
     })
 
     socket.on('disconnect', () => {
