d798d2e902574a53d9adbbf24d518d11d62d2a6a
igrepinc <igor.repinc@oroundo.com>
2019-01-16 12:01:48 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+const socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: 'Yup, that works for me'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+    console.log('New email', email);
+});
+
+socket.on('newMessage', function (message) {
+    console.log('NewMessage', message);
+});
+
